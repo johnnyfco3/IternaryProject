7cd9b801ccdd631149f906340c108ed33459e544
johnnytejada <johnnyfran20002@gmail.com>
2022-03-05 20:03:46 -0500

    adding features to welcome

--- iternary-app/src/Components/WelcomeBar.js
+++ iternary-app/src/Components/WelcomeBar.js
@@ -8,15 +8,18 @@ export function WelcomeBar(){
                 <div className="container">
                     <Link className="navbar-brand" to="/">Vacay</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <i className="fas fa-bars icon"></i>
+                        <i className="fas fa-bars icon"></i>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                        <a className="nav-link" href="#about">About</a>
+                            <a className="nav-link" href="#about">About</a>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="/login">Sign In</Link>
+                            <a className="nav-link" href="#design">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Sign In</Link>
                         </li>
                     </ul>
                     </div>
