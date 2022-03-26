fec09a0782f777627328cef778f8ae93e1d1a121
johnnytejada <johnnyfran20002@gmail.com>
2022-03-25 23:47:22 -0400

    adding to preview

--- iternary-app/src/Components/Preview.js
+++ iternary-app/src/Components/Preview.js
@@ -2,31 +2,26 @@ import React from "react";
 import MainPage from "../Images/MainPage.png"
 import Adventures from "../Images/Adventures.png"
 import Friends from "../Images/Friends.png"
+import PostCards from "../Images/PostCards.png"
 
 export function Preview(){
     return (
         <div className="container reveal">
             <h1 className="title text-center pb-5 pt-2">Design Your Page To Your <br /> Preference</h1>
-            <div className="row pt-5">
-                <div className="col-4 pb-4">
+            <div className="row pt-2">
+                <div className="col-6 pb-4">
                     <img src={MainPage} alt="Main Page"/>
                 </div>
-                <div className="col-4 pb-4">
+                <div className="col-6 pb-4">
                     <img src={Adventures} alt="Adventures"/>
                 </div>
-                <div className="col-4 pb-4">
+            </div>
+            <div className="row pt-4">
+                <div className="col-6 pb-4">
                     <img src={Friends} alt="Friends"/>
                 </div>
-            </div>
-            <div className="row pt-2">
-                <div className="col-4">
-                    <h3 className="text-center">Main Page</h3>
-                </div>
-                <div className="col-4">
-                    <h3 className="text-center">Adventures</h3>
-                </div>
-                <div className="col-4">
-                    <h3 className="text-center">Friends</h3>
+                <div className="col-6 pb-4">
+                    <img src={PostCards} alt="Post Cards"/>
                 </div>
             </div>
         </div>  
